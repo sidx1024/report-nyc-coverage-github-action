d35faf97525bc52bdf5bf52c8962ac5cfa323693
sid <vistax86live@gmail.com>
2022-03-16 17:10:39 +0530

    More setup

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const { replaceTokens } = require("./utils");
+const { replaceTokens } = require('./utils');
 
 try {
+  const whoToGreet = core.getInput('who-to-greet');
+  const time = new Date().toTimeString();
+
   // `who-to-greet` input defined in action metadata file
   const tokenMap = {
-    'who-to-greet': core.getInput('who-to-greet'),
-    'time': (new Date()).toTimeString(),
-  }
+    'who-to-greet': whoToGreet,
+    time: time,
+  };
   console.log(replaceTokens(`Hello {{who-to-greet}}, the time is {{time}}`, tokenMap));
-
+  core.setOutput('time', time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
