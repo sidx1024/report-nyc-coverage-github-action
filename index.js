00db478d137a7fdc775753560d0c9213f73c4332
sid <vistax86live@gmail.com>
2022-03-16 22:41:59 +0530

    Calculate coverage

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const path = require('path');
+const fs = require('fs');
+// const os = require('os');
 
 const { replaceTokens } = require('./utils');
 
-try {
-  const whoToGreet = core.getInput('who-to-greet');
-  const time = new Date().toTimeString();
+async function run() {
+  // const tmpPath = path.resolve(os.tmpdir(), github.context.action);
+  const coverageOutputDirectory = core.getInput('coverage-output-directory');
 
-  // `who-to-greet` input defined in action metadata file
-  const tokenMap = {
-    'who-to-greet': whoToGreet,
-    time: time,
-  };
-  console.log(replaceTokens(`Hello {{who-to-greet}}, the time is {{time}}`, tokenMap));
-  core.setOutput('time', time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+  const coverageSummaryJSONPath = path.resolve(coverageOutputDirectory, 'coverage-summary.json');
+  const coverageSummaryJSON = JSON.parse(fs.readFileSync(coverageSummaryJSONPath, 'utf-8'));
+
+  const summary = parseCoverageSummaryJSON(coverageSummaryJSON);
+
+  core.setOutput('total_lines_coverage_percent', summary.total_lines_coverage_percent);
 }
+
+run().catch((error) => {
+  core.setFailed(error.message);
+});
