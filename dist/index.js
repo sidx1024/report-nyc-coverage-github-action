f7259445b199367e7959b3d18b6e03c4b3f8df0b
sid <vistax86live@gmail.com>
2022-03-22 14:11:34 +0530

    Add coverage level indicators

--- dist/index.js
+++ dist/index.js
@@ -9613,10 +9613,17 @@ const LETTER_LABEL = {
 };
 
 const LETTER_PERCENT = {
-  S: (data) => addPercentSignOrReturnEmptyString(data.statements.pct),
-  B: (data) => addPercentSignOrReturnEmptyString(data.branches.pct),
-  F: (data) => addPercentSignOrReturnEmptyString(data.functions.pct),
-  L: (data) => addPercentSignOrReturnEmptyString(data.lines.pct),
+  S: (data) => formatPercentWithIndicator(data.statements.pct),
+  B: (data) => formatPercentWithIndicator(data.branches.pct),
+  F: (data) => formatPercentWithIndicator(data.functions.pct),
+  L: (data) => formatPercentWithIndicator(data.lines.pct),
+};
+
+const COVERAGE_LEVEL_IMAGE = {
+  low: 'https://user-images.githubusercontent.com/11299391/159438929-80263e3a-24e9-473d-93da-ff58e067fd7e.svg',
+  medium:
+    'https://user-images.githubusercontent.com/11299391/159438920-d818f420-0b6a-4c28-93bd-7fb5ff2253dd.svg',
+  high: 'https://user-images.githubusercontent.com/11299391/159438926-64f0e248-5533-4c17-9f9b-9628cc6b5696.svg',
 };
 
 function formatFilesCoverageDataToHTMLTable(filesCoverageData, options = {}) {
@@ -9646,8 +9653,26 @@ function formatFilesCoverageDataToHTMLTable(filesCoverageData, options = {}) {
   return createHTMLTableFromArray([headers, ...rows]);
 }
 
-function addPercentSignOrReturnEmptyString(input) {
-  return Number.isFinite(input) ? input + '%' : '';
+function formatPercentWithIndicator(percent) {
+  if (!Number.isFinite(percent)) {
+    return '';
+  }
+
+  const imageSrc = getCoverageLevelImage(percent);
+  const imageHTML = `<img src="${imageSrc}">`;
+
+  return imageHTML + '&nbsp;' + percent + '%';
+}
+
+function getCoverageLevelImage(percent) {
+  // https://github.com/istanbuljs/istanbuljs/blob/c1559005b3bb318da01f505740adb0e782aaf14e/packages/istanbul-lib-report/lib/watermarks.js
+  if (percent >= 80) {
+    return COVERAGE_LEVEL_IMAGE.high;
+  } else if (percent >= 50) {
+    return COVERAGE_LEVEL_IMAGE.medium;
+  } else {
+    return COVERAGE_LEVEL_IMAGE.low;
+  }
 }
 
 function createLink(link, label) {
